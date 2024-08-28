1b3eec22f28f19b59387c4ff13b2d78c56252623
siddhantprateek <siddhantprateek@gmail.com>
2024-08-28 19:28:42 +0530

    updated post duration

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 40s;
+sleep 90s;
 
 docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_TYPE --value 2
 docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SERVER --value "$PS_MAIL_SERVER"
