4e4799a76dc8ec2ad75a294c15eb52a6ec32039d
siddhantprateek <siddhantprateek@gmail.com>
2024-08-29 11:46:07 +0530

    email config added to docs

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ Once the installation is complete, you can access your PrestaShop Admin dashboar
 
     URL: https://[CI_CD_DOMAIN]/admin_ps
 
+
+## Configure Email configuration for Prestashop
+
+Open the terminal or Head towards Tools VS Code to open in editor, and run the following commands
+
+    set -o allexport; source .env; set +o allexport;
+
+    docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SERVER --value "$PS_MAIL_SERVER"
+    docker compose exec -it prestashop bin/console prestashop:config set PS_SHOP_EMAIL --value "$ADMIN_EMAIL"
+    docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_USER --value "$PS_MAIL_USER"
+    docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_PASSWD --value "$PS_MAIL_PASSWD"
+    docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_PORT --value "$PS_MAIL_SMTP_PORT"
+    docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_ENCRYPTION --value "tls"
+
+After the email configuration is set, Log into your Prestashop Admin console, go to Advance Parameters on your Sidebar, select Email
+Under Email Section, Choose `Set my Own SMTP Parameters (For Advance users only)` and save the configuration.
+
+<img src="prestashop-email.png" style='width: 100%;'/>
--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,12 +3,12 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 200s;
+sleep 120s;
 
-docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SERVER --value "$PS_MAIL_SERVER"
-docker compose exec -it prestashop bin/console prestashop:config set PS_SHOP_EMAIL --value "$ADMIN_EMAIL"
-docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_USER --value "$PS_MAIL_USER"
-docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_PASSWD --value "$PS_MAIL_PASSWD"
-docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_PORT --value "$PS_MAIL_SMTP_PORT"
-docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_ENCRYPTION --value "tls"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SERVER --value "$PS_MAIL_SERVER"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_SHOP_EMAIL --value "$ADMIN_EMAIL"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_USER --value "$PS_MAIL_USER"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_PASSWD --value "$PS_MAIL_PASSWD"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_PORT --value "$PS_MAIL_SMTP_PORT"
+# docker compose exec -it prestashop bin/console prestashop:config set PS_MAIL_SMTP_ENCRYPTION --value "tls"
 
